78545d3a505d369b6930ba376ebde9f3f523ef6b
icebox827 <icebox827@gmail.com>
2020-08-05 19:30:22 -0400

    backpack config correction

--- app/User.php
+++ app/User.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
+
+
 {
     use Notifiable;
 
@@ -36,4 +38,5 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+   
 }
